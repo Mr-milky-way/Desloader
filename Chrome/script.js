b05287b3d722996f819f4d6758abd63cc523f549
Mr-milky-way <admilkyway@gmail.com>
2025-06-29 19:13:16 -0700

    Added Load OBJ Optimized with array for Rotations.

--- Chrome/script.js
+++ Chrome/script.js
@@ -19,21 +19,17 @@ function run() {
         div.innerHTML = `
             <div id="main_bit">
                 <button onclick="hide()">Hide</button>
-                <h4>Save State</h4>
-                <button onclick="downloadState(Calc)">Save State</button>
-                <h4>Load State</h4>
-                <input type="file" id="loadState" accept=".des" />
-                <h4>Color</h4>
-                <input type="text" id="color" placeholder="Color Variable Name">
-                <h4>Load OBJ</h4>
-                <input type="file" id="loadOBJ" accept=".obj" />
-                <h4>Load OBJ Optimized</h4>
-                <input type="file" id="loadOBJ_faster" accept=".obj" />
-                <h5>Load OBJ Optimized with array for placement? Do all this before uploading file. <input type="checkbox" id="ArrayCheckBox"></h5>
-                <input type="text" id="ArrayName" placeholder="Array Name">
+                <h4>Save State <button onclick="downloadState(Calc)">Save State</button></h4>
+                <h4>Load State <input type="file" id="loadState" accept=".des" /></h4>
+                <h4>Color <input type="text" id="color" placeholder="Color Variable Name"></h4>
+                <h4>Load OBJ <input type="file" id="loadOBJ" accept=".obj" /></h4>
+                <h4>Load OBJ Optimized <input type="file" id="loadOBJ_faster" accept=".obj" /></h4>
+                <h5>Load OBJ Optimized with array for placement? Do all this before uploading file. <input type="checkbox" id="PosArrayCheckBox"></h5>
+                <h5>Load OBJ Optimized with array for Rotations? (Only works in degrees) Do all this before uploading file. <input type="checkbox" id="RotArrayCheckBox"></h5>
+                <input type="text" id="ArrayName" placeholder="Array Name For Pos">
+                <input type="text" id="Array1Name" placeholder="Array Name For Rot">
                 <input type="text" id="ArrayCount" placeholder="Array Count">
-                <h4>Load Position Array from CSV<h5>Use the Array Name input from above</h5></h4>
-                <input type="file" id="LoadArray" accept=".csv" />
+                <h4>Load Position Array from CSV (Use the Array Name input from Pos) <input type="file" id="LoadArray" accept=".csv" /></h4>
             </div>
             <div id="show_bit">
                 <button onclick="show()">Show</button>
@@ -41,7 +37,7 @@ function run() {
         div.id = 'outside bit';
         div.style.position = 'absolute';
         div.style.top = '10px';
-        div.style.left = '17rem';
+        div.style.left = '20rem';
         div.style.zIndex = 9999;
         div.style.background = 'white';
         div.style.padding = '1px';
