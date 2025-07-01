066cc2a48506a4ecbd870d088e19faf1df2fadc1
Mr-milky-way <admilkyway@gmail.com>
2025-06-30 17:25:12 -0700

    Started on CSS

--- Chrome/script.js
+++ Chrome/script.js
@@ -17,32 +17,82 @@ function run() {
         (document.head || document.documentElement).appendChild(script);
         const div = document.createElement('div');
         div.innerHTML = `
-            <div id="main_bit">
+            <style>
+                .main .button1 {
+                    color: rgb(0, 0, 0);
+                }
+                .button:hover {
+                    background-color:rgb(0, 140, 187);
+                }
+                .show {
+                    background: rgba(255,255,255,0.0);
+                }
+                .main {
+                    background: rgb(42, 42, 42);
+                    color: rgb(255,255,255);
+                }
+                h3 {
+                    color: rgb(255,255,255);
+                }
+                h4 {
+                    color: rgb(255, 255, 255);
+                }
+                h4 .button1 {
+                    color: rgb(0, 0, 0);
+                }
+                h5 {
+                    color: rgb(255,255,255);
+                }
+                .text1 {
+                    text-align: center;
+                    border-radius: 10px;
+                    border: none;
+                    width: 30%;
+                    max-width: 600px;
+                    color: rgb(255,255,255);
+                    background-color: rgb(54, 54, 54);
+                }
+                .text1 input {
+                    padding: 1rem;
+                    font-size: 1.2rem;
+                    width: 30%;
+                    max-width: 400px;
+                    border-radius: 5px;
+                    border: none;
+                    background-color: #333;
+                    color: rgb(255,255,255);
+                    transition: transform 0.3s ease;
+                }
+                .text1::placeholder {
+                    color: rgb(255,255,255);
+                    opacity: .8;
+                }
+            </style>
+            <div class="main" id="main_bit">
                 <h3>Tools</h3>
-                <button onclick="hide()">Hide</button>
-                <h4>Save State <button onclick="downloadState(Calc)">Save State</button></h4>
+                <button class="button1" onclick="hide()">Hide</button>
+                <h4>Save State <button class="button1" onclick="downloadState(Calc)">Save State</button></h4>
                 <h4>Load State <input type="file" id="loadState" accept=".des" /></h4>
-                <h4>Color <input type="text" id="color" placeholder="Color Variable Name"></h4>
+                <h4>Color <input class="text1" type="text" id="color" placeholder="Color Variable Name"></h4>
                 <h4>Load OBJ <input type="file" id="loadOBJ" accept=".obj" /></h4>
                 <h4>Load OBJ Optimized <input type="file" id="loadOBJ_faster" accept=".obj" /></h4>
                 <h5>Load OBJ Optimized with UV map? Do all this before uploading file. <input type="checkbox" id="UVmapCheckBox"></h5>
                 <h5>Load OBJ Optimized with array for Placement? Do all this before uploading file. <input type="checkbox" id="PosArrayCheckBox"></h5>
                 <h5>Load OBJ Optimized with array for Rotations? (Only works in degrees) Do all this before uploading file. <input type="checkbox" id="RotArrayCheckBox"></h5>
-                <input type="text" id="ArrayName" placeholder="Array Name For Placement">
-                <input type="text" id="Array1Name" placeholder="Array Name For Rotations">
-                <input type="text" id="ArrayCount" placeholder="Array Count">
+                <input class="text1" type="text" id="ArrayName" placeholder="Array Name For Placement">
+                <input class="text1" type="text" id="Array1Name" placeholder="Array Name For Rotations">
+                <input class="text1" type="text" id="ArrayCount" placeholder="Array Count">
                 <h4>Load Array from CSV (Use the Array Name input from Placement) <input type="file" id="LoadArray" accept=".csv" /></h4>
                 <h4>Input Texture File (Only work on the most recent OBJ Optimized with UV map) <input type="file" id="upload" accept=".png" /></h4>
             </div>
-            <div id="show_bit">
-                <button onclick="show()">Show</button>
+            <div class="show" id="show_bit">
+                <button class="button1" onclick="show()">Show</button>
             </div>`;
         div.id = 'outside bit';
         div.style.position = 'absolute';
         div.style.top = '10px';
         div.style.left = '20rem';
         div.style.zIndex = 9999;
-        div.style.background = 'white';
         div.style.padding = '1px';
         document.body.appendChild(div);
     }, 1000);
